335397db387e9846eebe8108616c0066d3a29475
curious-abdulghaffar <abdulghaffar4324@gmail.com>
2025-10-18 22:55:55 +0500

    menu bar added

---
+++ javascript/app.js
@@ -0,0 +1,15 @@
+const burgerMenu = document.querySelector(".burger");
+
+const menuBar = document.querySelector(".menu-bar");
+
+function showMenu(){
+menuBar.style.display = "flex";
+console.log(burgerMenu)
+// alert("hellooooo")
+}
+
+function hideMenu(){
+    menuBar.style.display = "none";
+    console.log(burgerMenu)
+    // alert("hellooooo")
+    }
